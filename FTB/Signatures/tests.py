3b0582c4575f960b4a73e254603e53a9170a7909
Christian Holler (:decoder) <choller@mozilla.com>
2014-10-03 17:24:07 +0200

    Header fixing

--- FTB/Signatures/tests.py
+++ FTB/Signatures/tests.py
@@ -1,7 +1,15 @@
 '''
-Created on Oct 1, 2014
+Tests
 
-@author: decoder
+@author:     Christian Holler (:decoder)
+
+@license:
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+@contact:    choller@mozilla.com
 '''
 import unittest
 from FTB.Signatures.CrashInfo import ASanCrashInfo, GDBCrashInfo, CrashInfo
