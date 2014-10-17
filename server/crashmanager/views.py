8c187a7cebf71cef795409f2139641279f802f7d
Christian Holler (:decoder) <choller@mozilla.com>
2014-10-17 19:04:38 +0200

    Add two REST views and route URLs accordingly

--- server/crashmanager/views.py
+++ server/crashmanager/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from crashmanager.serializers import BucketSerializer, CrashEntrySerializer
+from crashmanager.models import CrashEntry, Bucket
 
-# Create your views here.
+
+class CrashEntryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows adding/viewing CrashEntries
+    """
+    queryset = CrashEntry.objects.all()
+    serializer_class = CrashEntrySerializer
+
+
+class BucketViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    API endpoint that allows reading of signatures
+    """
+    queryset = Bucket.objects.all()
+    serializer_class = BucketSerializer
--- server/server/urls.py
+++ server/server/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, include, url
+from rest_framework import routers
+from crashmanager import views
 
 from django.contrib import admin
 admin.autodiscover()
+
+router = routers.DefaultRouter()
+router.register(r'signatures', views.BucketViewSet)
+router.register(r'crashes', views.CrashEntryViewSet)
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,5 +15,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^', include(router.urls)),
 )
