7d8232e2bb2481c56fb8fe682ede2440a1d62775
Christian Holler (:decoder) <choller@mozilla.com>
2014-10-17 19:03:25 +0200

    Add default permissions for REST framework

--- server/server/settings.py
+++ server/server/settings.py
@@ -82,3 +82,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
