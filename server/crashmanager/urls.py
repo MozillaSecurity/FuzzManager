715a417f6274395f7fedfff80ce55dad602283fe
Christian Holler (:decoder) <choller@mozilla.com>
2014-10-24 15:24:26 +0200

    Add URL entries for signature/crash views, route the REST interface to rest/ (#16)

---
+++ server/crashmanager/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+from rest_framework import routers
+from crashmanager import views
+
+router = routers.DefaultRouter()
+router.register(r'signatures', views.BucketViewSet)
+router.register(r'crashes', views.CrashEntryViewSet)
+
+urlpatterns = patterns('',
+    url(r'^rest/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^logout/$', views.logout_view, name='logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^$', views.index, name='index'),
+    url(r'^signatures/$', views.signatures, name='signatures'),
+    url(r'^signatures/new/$', views.newSignature, name='signew'),
+    url(r'^signatures/(?P<sigid>\d+)/edit/$', views.editSignature, name='sigedit'),
+    url(r'^signatures/(?P<sigid>\d+)/linkextbug/$', views.editSignature, name='linkextbug'),
+    url(r'^signatures/(?P<sigid>\d+)/view/$', views.viewSignature, name='sigview'),
+    url(r'^signatures/(?P<sigid>\d+)/delete/$', views.deleteSignature, name='sigdel'),
+    url(r'^crashes/$', views.crashes, name='crashes'),
+    url(r'^rest/', include(router.urls)),
+)
--- server/server/urls.py
+++ server/server/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from rest_framework import routers
 from crashmanager import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -15,6 +16,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include(router.urls)),
+    url(r'^crashmanager/', include('crashmanager.urls', namespace="crashmanager")),
 )
+
+urlpatterns += staticfiles_urlpatterns()
