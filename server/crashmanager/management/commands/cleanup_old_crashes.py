1a804bba0ee553cd87d29599284c1b422ad28196
Christian Holler (:decoder) <choller@mozilla.com>
2014-11-28 21:29:16 +0100

    Refactor cleanup command to use settings.py

--- server/crashmanager/management/commands/cleanup_old_crashes.py
+++ server/crashmanager/management/commands/cleanup_old_crashes.py
@@ -2,15 +2,17 @@ from django.core.management.base import NoArgsCommand
 from crashmanager.models import CrashEntry, Bucket, Bug
 from django.db.models.aggregates import Count
 from datetime import datetime, timedelta
-
-CLEANUP_CRASHES_AFTER_DAYS = 14
-CLEANUP_FIXED_BUCKETS_AFTER_DAYS = 3
+from django.conf import settings
 
 class Command(NoArgsCommand):
     help = "Cleanup old crash entries."
     def handle_noargs(self, **options):
+        cleanup_crashes_after_days = getattr(settings, 'CLEANUP_CRASHES_AFTER_DAYS', 14)
+        cleanup_fixed_buckets_after_days = getattr(settings, 'CLEANUP_FIXED_BUCKETS_AFTER_DAYS', 3)
+
+        
         # Select all buckets that have been closed for x days
-        expiryDate = datetime.now().date() - timedelta(days=CLEANUP_FIXED_BUCKETS_AFTER_DAYS)
+        expiryDate = datetime.now().date() - timedelta(days=cleanup_fixed_buckets_after_days)
         bugs = Bug.objects.filter(closed__lt = expiryDate)
         for bug in bugs:
             # This delete causes buckets referring to this bug, as well as entries
@@ -23,7 +25,7 @@ class Command(NoArgsCommand):
             bucket.delete()
             
         # Select all entries that are older than x days
-        expiryDate = datetime.now().date() - timedelta(days=CLEANUP_CRASHES_AFTER_DAYS)
+        expiryDate = datetime.now().date() - timedelta(days=cleanup_crashes_after_days)
         entries = CrashEntry.objects.filter(created__lt = expiryDate)
         for entry in entries:
             entry.delete()
