cec937d9cd16d8fa7cfd69cd5c7bc9ae54af5661
Christian Holler (:decoder) <choller@mozilla.com>
2015-01-27 16:36:40 +0100

    ProgramConfiguration: Add a method to handle binary configuration files (#62)

--- FTB/ProgramConfiguration.py
+++ FTB/ProgramConfiguration.py
@@ -19,9 +19,12 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # Ensure print() compatibility with Python 3
 from __future__ import print_function
+import sys
+import os
+from FTB.ConfigurationFiles import ConfigurationFiles
 
 class ProgramConfiguration():
-    def __init__(self, product, platform, os, version=None, env=None, args=None):
+    def __init__(self, product, platform, os, version=None, env=None, args=None, metadata=None):
         '''
         @type product: string
         @param product: The name of the product/program/branch tested
@@ -40,9 +43,67 @@ class ProgramConfiguration():
         
         if args is None:
             args = []
+            
+        if metadata is None:
+            metadata = {}
         
         assert isinstance(env, dict)
         assert isinstance(args, list)
+        assert isinstance(metadata, dict)
         
         self.env = env
         self.args = args
+        self.metadata = metadata
+    
+    @staticmethod
+    def fromBinary(binaryPath):
+        binaryConfig = "%s.fuzzmanagerconf" % binaryPath
+        if not os.path.exists(binaryConfig):
+            print("Warning: No binary configuration found at %s" % binaryConfig, file=sys.stderr)
+            return None
+
+        config = ConfigurationFiles([ binaryConfig ])
+        mainConfig = config.mainConfig
+        
+        for field in ["product", "platform", "os"]:
+            if not field in mainConfig:
+                raise RuntimeError('Missing "%s" in binary configuration file %s' % (field, binaryConfig))
+            
+        # Version field is optional
+        version = None
+        if "product_version" in mainConfig:
+            version = mainConfig["product_version"]
+        
+        return ProgramConfiguration(mainConfig["product"], mainConfig["platform"], mainConfig["os"], version=version, metadata=config.metadataConfig)
+    
+    def addEnvironmentVariables(self, env):
+        '''
+        Add (additional) environment variable definitions. Existing definitions
+        will be overwritten if they are redefined in the given environment.
+        
+        @type env: dict
+        @param env: Dictionary containing the environment variables
+        '''
+        assert isinstance(env, dict)
+        self.env.update(env)
+        
+    def addProgramArguments(self, args):
+        '''
+        Add (additional) program arguments.
+        
+        @type args: list
+        @param args: List containing the program arguments
+        '''
+        assert isinstance(args, list)
+        self.args.extend(args)
+        
+    def addMetadata(self, metadata):
+        '''
+        Add (additional) metadata definitions. Existing definitions
+        will be overwritten if they are redefined in the given metadata.
+        
+        @type metadata: dict
+        @param metadata: Dictionary containing the metadata
+        '''
+        assert isinstance(metadata, dict)
+        self.metadata.update(metadata)
