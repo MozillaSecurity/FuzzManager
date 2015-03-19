24056fe29eee52aabab962b8f669a2d8f711801e
Christian Holler (:decoder) <choller@mozilla.com>
2015-03-19 16:31:28 +0100

    Add database migrations for tool addition and user-extension model (#74)

---
+++ server/crashmanager/migrations/0004_add_tool.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+def create_migration_tool(apps, schema_editor):
+    Tool = apps.get_model("crashmanager", "Tool")
+    db_alias = schema_editor.connection.alias
+    Tool.objects.using(db_alias).bulk_create([
+        Tool(name="migrated"),
+    ])
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('crashmanager', '0003_bucket_frequent'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Tool',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=63)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.RunPython(
+            create_migration_tool,
+        ),
+    ]
---
+++ server/crashmanager/migrations/0005_add_user.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+from crashmanager.models import Tool
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('crashmanager', '0004_add_tool'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('defaultTemplateId', models.IntegerField(default=0)),
+                ('defaultToolsFilter', models.ManyToManyField(to='crashmanager.Tool')),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='crashentry',
+            name='tool',
+            field=models.ForeignKey(default=1, to='crashmanager.Tool'),
+            preserve_default=False,
+        ),
+    ]
