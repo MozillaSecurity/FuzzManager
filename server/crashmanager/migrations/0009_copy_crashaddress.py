1fa4b95eb3d92fd9ba5554a8f43e85d0508bb905
Christian Holler (:decoder) <choller@mozilla.com>
2015-08-21 14:09:15 +0200

    Migrate CrashAddress pt2: Add mitigation for conversion (#119)

---
+++ server/crashmanager/migrations/0009_copy_crashaddress.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+import sys
+
+from django.db import models, migrations
+from django.conf import settings
+
+def create_migration_tool(apps, schema_editor):
+    CrashEntry = apps.get_model("crashmanager", "CrashEntry")
+    
+    for entry in CrashEntry.objects.filter(crashAddressNumeric = None):
+        if entry.crashAddress == None or len(entry.crashAddress) == 0:
+            entry.crashAddressNumeric = None
+            entry.save()
+        else:
+            try:
+                entry.crashAddressNumeric = long(entry.crashAddress, 16)
+                entry.save()
+            except ValueError as e:
+                print("Failed to convert crash address value: %s" % entry.crashAddress, file=sys.stderr)
+    
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crashmanager', '0008_crashentry_crashaddressnumeric'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            create_migration_tool,
+        ),
+    ]
