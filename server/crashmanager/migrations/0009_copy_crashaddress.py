05c588866cc66bff33cb77fe35434f850ddd07f0
Christian Holler (:decoder) <choller@mozilla.com>
2015-08-21 14:34:14 +0200

    Handle values larger than 2**63-1 in numeric crash address conversion (#119)

--- server/crashmanager/migrations/0009_copy_crashaddress.py
+++ server/crashmanager/migrations/0009_copy_crashaddress.py
@@ -16,6 +16,13 @@ def create_migration_tool(apps, schema_editor):
         else:
             try:
                 entry.crashAddressNumeric = long(entry.crashAddress, 16)
+                
+                # Crash addresses are typically unsigned, but our database
+                # can only store signed 64 bit integers. Convert to signed
+                # if the value exceeds maximum value for signed 64 bit.
+                if (entry.crashAddressNumeric > (2**63-1)):
+                    entry.crashAddressNumeric -= 2**64
+                    
                 entry.save()
             except ValueError as e:
                 print("Failed to convert crash address value: %s" % entry.crashAddress, file=sys.stderr)
