963d2b7ab60da39d2ceee1304055527767ea4939
Christian Holler (:decoder) <choller@mozilla.com>
2015-12-18 02:26:07 +0100

    Style adjustments in models.py

--- server/crashmanager/models.py
+++ server/crashmanager/models.py
@@ -1,17 +1,17 @@
 from django.conf import settings
-from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import User as DjangoUser
 from django.core.files.storage import FileSystemStorage
+from django.db import models
 from django.db.models.signals import post_delete
 from django.dispatch.dispatcher import receiver
-from django.contrib.auth.models import User as DjangoUser
-
-from FTB.Signatures.CrashSignature import CrashSignature
-from FTB.Signatures.CrashInfo import CrashInfo
-from FTB.ProgramConfiguration import ProgramConfiguration
-
+from django.utils import timezone
 import json
 import re
+
+from FTB.ProgramConfiguration import ProgramConfiguration
+from FTB.Signatures.CrashInfo import CrashInfo
+from FTB.Signatures.CrashSignature import CrashSignature
+
 
 class Tool(models.Model):
     name = models.CharField(max_length=63)
@@ -22,32 +22,32 @@ class Platform(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=63)
     version = models.CharField(max_length=127, blank=True, null=True)
-    
+
 class OS(models.Model):
     name = models.CharField(max_length=63)
     version = models.CharField(max_length=127, blank=True, null=True)
-    
+
 class TestCase(models.Model):
     test = models.FileField(storage=FileSystemStorage(location=getattr(settings, 'TEST_STORAGE', None)), upload_to="tests")
     size = models.IntegerField(default=0)
     quality = models.IntegerField(default=0)
     isBinary = models.BooleanField(default=False)
-    
+
     def __init__(self, *args, **kwargs):
         # This variable can hold the testcase data temporarily
         self.content = None
-        
+
         # For performance reasons we do not load the test here
         # automatically. You must call the loadTest method if you
         # want to access the test content
-        
+
         super(TestCase, self).__init__(*args, **kwargs)
-    
+
     def loadTest(self):
         self.test.open(mode='rb')
         self.content = self.test.read()
         self.test.close()
-        
+
     def storeTestAndSave(self):
         self.size = len(self.content)
         self.test.open(mode='w')
@@ -57,14 +57,14 @@ class TestCase(models.Model):
 
 class Client(models.Model):
     name = models.CharField(max_length=255)
-    
+
 class BugProvider(models.Model):
     classname = models.CharField(max_length=255, blank=False)
     hostname = models.CharField(max_length=255, blank=False)
-    
+
     # This is used to annotate bugs with the URL linking to them
     urlTemplate = models.CharField(max_length=1023, blank=False)
-    
+
     def getInstance(self):
         # Dynamically instantiate the provider as requested
         providerModule = __import__('crashmanager.Bugtracker.%s' % self.classname, fromlist=[self.classname])
@@ -85,7 +85,7 @@ class Bucket(models.Model):
 
     def getSignature(self):
         return CrashSignature(self.signature)
-    
+
     def save(self, *args, **kwargs):
         # Sanitize signature line endings so we end up with the same hash
         # TODO: We might want to just parse the JSON here, and re-serialize
@@ -118,27 +118,26 @@ class CrashEntry(models.Model):
         self.argsList = None
         self.envList = None
         self.metadataList = None
-        
+
         # For performance reasons we do not deserialize these fields
-        # automatically here. You need to explicitly call the 
+        # automatically here. You need to explicitly call the
         # deserializeFields method if you need this data.
-        
+
         super(CrashEntry, self).__init__(*args, **kwargs)
-        
+
     def save(self, *args, **kwargs):
         if self.pk is None and not getattr(settings, 'DB_ISUTF8MB4', False):
             # Replace 4-byte UTF-8 characters with U+FFFD if our database
-            # doesn't support them. By default, MySQL utf-8 does not support these.  
+            # doesn't support them. By default, MySQL utf-8 does not support these.
             utf8_4byte_re = re.compile(u'[^\u0000-\uD7FF\uE000-\uFFFF]', re.UNICODE)
             def sanitize_utf8(s):
                 if not isinstance(s, unicode):
                     s = unicode(s, 'utf-8')
                 return utf8_4byte_re.sub(u"\uFFFD", s)
-            
+
             self.rawStdout = sanitize_utf8(self.rawStdout)
             self.rawStderr = sanitize_utf8(self.rawStderr)
             self.rawCrashData = sanitize_utf8(self.rawCrashData)
-        
 
         if not self.cachedCrashInfo:
             # Serialize the important fields of the CrashInfo class into a JSON blob
@@ -148,43 +147,43 @@ class CrashEntry(models.Model):
         # Reserialize data, then call regular save method
         if self.argsList:
             self.args = json.dumps(self.argsList)
-    
+
         if self.envList:
             envDict = dict([x.split("=", 1) for x in self.envList])
             self.env = json.dumps(envDict)
-    
+
         if self.metadataList:
             metadataDict = dict([x.split("=", 1) for x in self.metadataList])
             self.metadata = json.dumps(metadataDict)
-        
+
         # When we have a crash address, keep the numeric crash address field in
         # sync so we can search easily by crash address including ranges
         if self.crashAddress:
             self.crashAddressNumeric = long(self.crashAddress, 16)
-            
+
             # We need to possibly convert the numeric crash address from unsigned
             # to signed in order to store it in the database.
-            if (self.crashAddressNumeric > (2**63-1)):
-                self.crashAddressNumeric -= 2**64
+            if (self.crashAddressNumeric > (2 ** 63 - 1)):
+                self.crashAddressNumeric -= 2 ** 64
 
         if len(self.shortSignature) > 255:
             self.shortSignature = self.shortSignature[0:255]
-        
+
         super(CrashEntry, self).save(*args, **kwargs)
-    
+
     def deserializeFields(self):
         if self.args:
             self.argsList = json.loads(self.args)
-    
+
         if self.env:
             envDict = json.loads(self.env)
-            self.envList = ["%s=%s" % (s,envDict[s]) for s in envDict.keys()]
-    
+            self.envList = ["%s=%s" % (s, envDict[s]) for s in envDict.keys()]
+
         if self.metadata:
             metadataDict = json.loads(self.metadata)
-            self.metadataList = ["%s=%s" % (s,metadataDict[s]) for s in metadataDict.keys()]
-    
-    
+            self.metadataList = ["%s=%s" % (s, metadataDict[s]) for s in metadataDict.keys()]
+
+
     def getCrashInfo(self, attachTestcase=False):
         # TODO: This should be cached at some level
         # TODO: Need to include environment and program arguments here
@@ -199,7 +198,7 @@ class CrashEntry(models.Model):
         if attachTestcase and self.testcase != None and not self.testcase.isBinary:
             self.testcase.loadTest()
             crashInfo.testcase = self.testcase.content
-        
+
         return crashInfo
 
 # This post_delete handler ensures that the corresponding testcase
