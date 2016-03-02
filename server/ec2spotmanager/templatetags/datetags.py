c9e4d79bb79f7350a62a3124994638c038a23d88
Christian Holler (:decoder) <choller@mozilla.com>
2016-03-02 01:16:08 +0100

    [EC2SpotManager] Fix timezone awareness in date_ago (#169)

--- server/ec2spotmanager/templatetags/datetags.py
+++ server/ec2spotmanager/templatetags/datetags.py
@@ -1,22 +1,34 @@
-import datetime
+from datetime import tzinfo, timedelta, datetime
 from django import template
+
+class UTC(tzinfo):
+    def utcoffset(self, dt):
+        return timedelta(0)
+    def tzname(self, dt):
+        return "UTC"
+    def dst(self, dt):
+        return timedelta(0)
 
 register = template.Library()
 
 @register.filter
 def date_ago(d):
-    delta = datetime.datetime.now() - d
+    delta = datetime.now(UTC()) - d
+
+    days = delta.days
+    hours = delta.seconds // 3600
+    minutes = delta.seconds // 60 % 60
 
     ret = ""
 
-    if delta.days > 0:
-        ret += "%s %s " % (delta.days, "days" if delta.days > 1 else "day")
+    if days > 0:
+        ret += "%s %s " % (days, "days" if days > 1 else "day")
 
-    if delta.hours > 0:
-        ret += "%s %s " % (delta.hours, "hours" if delta.hours > 1 else "hour")
+    if hours > 0:
+        ret += "%s %s " % (hours, "hours" if hours > 1 else "hour")
 
-    if delta.minutes > 0:
-        ret += "%s %s " % (delta.minutes, "minutes" if delta.minutes > 1 else "minute")
+    if minutes > 0:
+        ret += "%s %s " % (minutes, "minutes" if minutes > 1 else "minute")
 
     if not ret:
         ret = "less than a minute "
