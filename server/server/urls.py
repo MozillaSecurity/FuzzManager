1c951dd2bf0275e15e71e3723ddc690fdfae60dd
Christian Holler (:decoder) <choller@mozilla.com>
2016-08-22 18:24:01 +0200

    [FuzzManager] Redirect / to CrashManager by default (#223)

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,15 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'server.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'crashmanager.views.index', name='index'),
+    # url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^crashmanager/', include('crashmanager.urls', namespace="crashmanager", app_name='crashmanager')),
     url(r'^ec2spotmanager/', include('ec2spotmanager.urls', namespace="ec2spotmanager", app_name='ec2spotmanager')),
