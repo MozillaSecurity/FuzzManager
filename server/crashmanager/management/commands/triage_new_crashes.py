3c019d49e32819175d7afc716fac59e6441f154c
Christian Holler (:decoder) <choller@mozilla.com>
2016-09-21 14:43:00 +0200

    [CrashManager] Optimize triage_new_crashes command (#230)

--- server/crashmanager/management/commands/triage_new_crashes.py
+++ server/crashmanager/management/commands/triage_new_crashes.py
@@ -11,16 +11,30 @@ class Command(NoArgsCommand):
         entries = CrashEntry.objects.filter(triagedOnce=False, bucket=None)
         buckets = Bucket.objects.all()
 
-        for bucket in buckets:
-            signature = bucket.getSignature()
-            needTest = signature.matchRequiresTest()
+        signatureCache = {}
 
-            for entry in entries:
-                if signature.matches(entry.getCrashInfo(attachTestcase=needTest)):
+        for entry in entries:
+            # Benchmarks have shown that always attaching the testcase
+            # is faster than recreating the crash info in every iteration
+            # when signatures can be cached.
+            crashInfo = entry.getCrashInfo(attachTestcase=True)
+
+            for bucket in buckets:
+                if not bucket.pk in signatureCache:
+                    signatureCache[bucket.pk] = bucket.getSignature()
+
+                signature = signatureCache[bucket.pk]
+                matched = False
+
+                if signature.matches(crashInfo):
                     entry.bucket = bucket
+                    matched = True
 
                 entry.triagedOnce = True
                 entry.save()
+
+                if matched:
+                    break
 
         # This query ensures that all issues that have been bucketed manually before
         # the server had a chance to triage them will have their triageOnce flag set,
