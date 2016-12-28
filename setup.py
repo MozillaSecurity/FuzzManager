f463891a4b1eb63268d9d360943bdd2e508c6dde
Jesse Schwartzentruber <truber@mozilla.com>
2016-12-28 10:36:33 -0500

    Break out requirements for client so they aren't duplicated in setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@
 from setuptools import setup
 
 if __name__ == '__main__':
+    with open('requirements-client.txt') as requirements_fp:
+        requirements = requirements_fp.read().strip().splitlines()
     setup(name = "FuzzManager",
           version = "0.1",
           packages = ['Collector', 'FTB', 'FTB.Running', 'FTB.Signatures'],
-          install_requires = ['numpy==1.11.2', 'requests>=2.5.0'])
-
+          install_requires = requirements)
