3d2aab5b4fbc949b69ff324a9a6fcc090bd93c69
Christian Holler (:decoder) <choller@mozilla.com>
2017-08-05 14:28:07 +0200

    [CovManager][WIP] Experimental diff code for #319

--- server/covmanager/views.py
+++ server/covmanager/views.py
@@ -30,6 +30,10 @@ def collections_browse(request, collectionid):
     return render(request, 'collections/browse.html', { 'collectionid' : collectionid })
 
 @login_required(login_url='/login/')
+def collections_diff(request):
+    return render(request, 'collections/diff.html', {})
+
+@login_required(login_url='/login/')
 def collections_browse_api(request, collectionid, path):
     collection = get_object_or_404(Collection, pk=collectionid)
 
@@ -47,6 +51,86 @@ def collections_browse_api(request, collectionid, path):
         collection.annotateSource(path, coverage)
 
     data = { "path" : path, "coverage" : coverage }
+    return HttpResponse(json.dumps(data), content_type='application/json')
+
+@login_required(login_url='/login/')
+def collections_diff_api(request, path):
+
+    collections = None
+    coverages = []
+
+    if "ids" in request.GET:
+        ids = request.GET["ids"].split(",")
+        collections = Collection.objects.filter(pk__in=ids)
+
+    if len(collections) < 2:
+        raise Http404("Need at least two collections")
+
+    # coverage = collection.subset(path)
+
+    # if "children" in coverage:
+    #    # Viewing a directory, so we should remove detailed coverage
+    #    # information before returning this data.
+    #    Collection.strip(coverage)
+    # else:
+    #    raise Http404("NYI")
+
+    tooltipdata = []
+
+    for collection in collections:
+        coverage = collection.subset(path)
+
+        if "children" in coverage:
+            Collection.remove_childrens_children(coverage)
+
+            # Viewing a directory, so we should remove detailed coverage
+            # information before returning this data.
+            Collection.strip(coverage)
+        else:
+            # TODO: Check if the source file is identical in each collection
+            # If so, we can display it. If not, we should not annotate for now.
+            # collection.annotateSource(path, coverage)
+            raise Http404("NYI")
+
+        if not coverage:
+            raise Http404("Path not found.")
+
+        coverages.append(coverage)
+
+        ctooltipdata = {}
+        for k in coverage:
+            if k != "children":
+                ctooltipdata[k] = coverage[k]
+
+        if tooltipdata:
+            # Store delta to previous data point
+            print(type(ctooltipdata["coveragePercent"]))
+            print(type(tooltipdata[-1]["coveragePercent"]))
+            ctooltipdata["delta_coveragePercent"] = round(ctooltipdata["coveragePercent"] - tooltipdata[-1]["coveragePercent"], 4)
+
+            # TODO: More deltas?
+
+        tooltipdata.append(ctooltipdata)
+
+    start_coverage = coverages[0]
+    end_coverage = coverages[-1]
+
+    add = {}
+    for k in start_coverage:
+        if type(start_coverage[k]) == int or type(start_coverage[k]) == float:
+            add["delta_" + k] = end_coverage[k] - start_coverage[k]
+    start_coverage.update(add)
+
+    for child in start_coverage["children"]:
+        add = {}
+        for k in start_coverage["children"][child]:
+            x = start_coverage["children"][child][k]
+            # TODO: This assumes child is in end_coverage, which can fail
+            if type(x) == int or type(x) == float:
+                add["delta_" + k] = end_coverage["children"][child][k] - x
+        start_coverage["children"][child].update(add)
+
+    data = { "path" : path, "coverage" : start_coverage, "ttdata" : tooltipdata }
     return HttpResponse(json.dumps(data), content_type='application/json')
 
 class CollectionViewSet(mixins.CreateModelMixin,
