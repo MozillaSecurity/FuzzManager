927150804a6f2f92a4da6374694f3d11a4554f9b
Jesse Schwartzentruber <truber@mozilla.com>
2017-09-05 15:59:30 -0400

    Add tests for signatures/new/ view.

--- server/crashmanager/tests/test_signatures.py
+++ server/crashmanager/tests/test_signatures.py
@@ -11,6 +11,7 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 '''
 
 import httplib
+import json
 import logging
 
 from django.core.urlresolvers import reverse
@@ -18,7 +19,7 @@ from django.core.files.base import ContentFile
 from django.contrib.auth.models import User
 
 from . import TestCase
-from ..models import Bug, BugProvider
+from ..models import Bucket, CrashEntry
 
 
 log = logging.getLogger("fm.crashmanager.tests.signatures")
@@ -167,3 +168,104 @@ class AllSignaturesViewTests(SignaturesViewTests):
         self.assertContains(response, "bucket #1")
         self.assertContains(response, "bucket #2")
         self.assertContains(response, self.entries_fmt % 2)
+
+
+class NewSignatureTests(TestCase):
+    name = "crashmanager:signew"
+
+    def test_no_login(self):
+        """Request without login hits the login redirect"""
+        path = reverse(self.name)
+        self.assertRedirects(self.client.get(path), '/login/?next=' + path)
+
+    def test_new(self):
+        """Create signature from scratch, not reassigning crashes"""
+        self.client.login(username='test', password='test')
+        crash = self.create_crash(shortSignature='crash #1', stderr="blah")
+        response = self.client.get(reverse(self.name))
+        self.assertEqual(response.status_code, httplib.OK)
+        self.assertContains(response, 'name="shortDescription"')
+        self.assertContains(response, 'name="signature"')
+        self.assertContains(response, 'name="frequent"')
+        self.assertContains(response, 'name="reassign"')
+        sig = json.dumps({
+            'symptoms': [
+                {"src": "stderr",
+                 "type": "output",
+                 "value": "/^blah/"}
+            ]
+        })
+        response = self.client.post(reverse(self.name), {'shortDescription': 'bucket #1',
+                                                         'signature': sig,
+                                                         'submit_save': '1'})
+        log.debug(response)
+        bucket = Bucket.objects.get(shortDescription='bucket #1')
+        crash = CrashEntry.objects.get(pk=crash.pk)  # re-read
+        self.assertIsNone(crash.bucket)
+        self.assertFalse(bucket.frequent)
+        self.assertFalse(bucket.permanent)
+        self.assertRedirects(response, reverse('crashmanager:sigview', kwargs={'sigid': bucket.pk}))
+
+    def test_new_w_crashes(self):
+        """Create signature from scratch and reassign crashes"""
+        self.client.login(username='test', password='test')
+        crash = self.create_crash(shortSignature='crash #1', stderr="blah")
+        sig = json.dumps({
+            'symptoms': [
+                {"src": "stderr",
+                 "type": "output",
+                 "value": "/^blah/"}
+            ]
+        })
+        response = self.client.post(reverse(self.name), {'shortDescription': 'bucket #1',
+                                                         'signature': sig,
+                                                         'reassign': '1',
+                                                         'submit_save': '1'})
+        log.debug(response)
+        bucket = Bucket.objects.get(shortDescription='bucket #1')
+        crash = CrashEntry.objects.get(pk=crash.pk)  # re-read
+        self.assertEqual(crash.bucket, bucket)
+        self.assertFalse(bucket.frequent)
+        self.assertFalse(bucket.permanent)
+        self.assertRedirects(response, reverse('crashmanager:sigview', kwargs={'sigid': bucket.pk}))
+
+    def test_preview(self):
+        """Preview signature with crash matching"""
+        self.client.login(username='test', password='test')
+        crash = self.create_crash(shortSignature='crash #1', stderr="blah")
+        sig = json.dumps({
+            'symptoms': [
+                {"src": "stderr",
+                 "type": "output",
+                 "value": "/^blah/"}
+            ]
+        })
+        response = self.client.post(reverse(self.name), {'shortDescription': 'bucket #1',
+                                                         'signature': sig,
+                                                         'reassign': '1'})
+        log.debug(response)
+        self.assertFalse(Bucket.objects.filter(shortDescription='bucket #1').exists())
+        in_list = response.context['inList']
+        out_list = response.context['outList']
+        self.assertEqual(len(in_list), 1)
+        self.assertEqual(len(out_list), 0)
+        crash = CrashEntry.objects.get(pk=crash.pk)  # re-read
+        self.assertIsNone(crash.bucket)
+        self.assertEqual(in_list[0], crash)
+
+    def test_new_from_crash(self):
+        """Create signature from crash"""
+        self.client.login(username='test', password='test')
+        stderr = "Program received signal SIGSEGV, Segmentation fault.\n#0  sym_a ()\n#1  sym_b ()"
+        crash = self.create_crash(shortSignature='crash #1', stderr=stderr)
+        response = self.client.get(reverse(self.name) + '?crashid=%d' % crash.pk)
+        self.assertEqual(response.status_code, httplib.OK)
+        self.assertContains(response, 'name="shortDescription"')
+        self.assertContains(response, 'name="signature"')
+        self.assertContains(response, 'name="frequent"')
+        self.assertContains(response, 'name="reassign"')
+        # check that it looks like a signature was created from our crash
+        self.assertContains(response, 'symptoms')
+        self.assertContains(response, 'stackFrames')
+        self.assertContains(response, 'sym_a')
+        self.assertContains(response, 'sym_b')
