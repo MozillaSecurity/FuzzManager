56d557ec5a514e7b91ce2ae47e425340d363ed97
Jesse Schwartzentruber <truber@mozilla.com>
2017-09-12 12:42:48 -0400

    Add test for logout function.

--- server/crashmanager/tests/test_crashmanager.py
+++ server/crashmanager/tests/test_crashmanager.py
@@ -10,6 +10,7 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 '''
 
+import httplib
 import logging
 
 from django.core.urlresolvers import reverse
@@ -30,3 +31,10 @@ class CrashManagerTests(TestCase):
         """Request of root url redirects to crashes view"""
         self.client.login(username='test', password='test')
         self.assertRedirects(self.client.get('/'), reverse('crashmanager:crashes'))
+
+    def test_logout(self):
+        """Logout url actually logs us out"""
+        self.client.login(username='test', password='test')
+        self.assertEqual(self.client.get(reverse('crashmanager:crashes')).status_code, httplib.OK)
+        self.client.get(reverse('crashmanager:logout'))
+        self.assertRedirects(self.client.get('/'), '/login/?next=/')
