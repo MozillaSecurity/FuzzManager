606853e8c49d040231ee486a2e8c6c2c44912351
Jesse Schwartzentruber <truber@mozilla.com>
2017-10-04 09:33:53 -0400

    Setup FuzzManager deployment to PyPI.
    This will automatically deploy new tagged versions of the FM client to
    PyPI. (eg. `git tag 0.x.y && git push --follow-tags`)
    
    This allows installation with pip: `pip install FuzzManager`

--- setup.py
+++ setup.py
@@ -2,7 +2,21 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    setup(name='FuzzManager',
-          version='0.1.1',
+    setup(classifiers=[
+              "Intended Audience :: Developers",
+              "Topic :: Software Development :: Testing",
+              "Topic :: Security",
+              "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+              "Programming Language :: Python :: 2",
+              "Programming Language :: Python :: 2.7",
+          ],
+          description="A fuzzing management tools collection",
+          install_requires=['fasteners>=0.14.1', 'numpy>=1.11.2', 'requests>=2.5.0'],
+          keywords="fuzz fuzzing security test testing",
+          license="MPL 2.0",
+          maintainer="Mozilla Fuzzing Team",
+          maintainer_email="fuzzing@mozilla.com",
+          name='FuzzManager',
           packages=['Collector', 'CovReporter', 'EC2Reporter', 'FTB', 'FTB.Running', 'FTB.Signatures', 'Reporter'],
-          install_requires=['fasteners>=0.14.1', 'numpy>=1.11.2', 'requests>=2.5.0'])
+          url="https://github.com/MozillaSecurity/FuzzManager",
+          version='0.1.1')
