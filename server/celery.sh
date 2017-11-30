65e0606c7e09245e245bd0456db41db7784f227d
Jesse Schwartzentruber <truber@mozilla.com>
2017-11-30 14:21:09 -0500

    Use INFO loglevel for celerybeat.

--- server/celery.sh
+++ server/celery.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
 
-celery -A celeryconf -l debug beat &
+celery -A celeryconf -l info beat &
 celery -A celeryconf -l info -c 4 -n cron@%h -Q cron worker &
 celery -A celeryconf -l info -n worker@%h -Q celery worker
