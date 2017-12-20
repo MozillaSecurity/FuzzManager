d139fb13faea24873c317ab1e0c2adc77d20d36f
Gary Kwong <gary@rumblingedge.com>
2017-12-20 02:45:40 -0800

    Split imports to multiple lines.

--- server/server/settings.py
+++ server/server/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os, sys
+import os
+import sys
 from django.conf import global_settings
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
