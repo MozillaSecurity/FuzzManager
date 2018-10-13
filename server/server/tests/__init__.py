d346129fb33f84eaa61ed48f3d4b4f9570062241
Jesse Schwartzentruber <truber@mozilla.com>
2018-10-12 23:27:51 -0400

    Fix Collector and EC2Reporter tests. Token user used for test now requires the correct permissions.

--- server/server/tests/__init__.py
+++ server/server/tests/__init__.py
@@ -1,6 +1,8 @@
 import pytest
-from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User, Permission
 from rest_framework.authtoken.models import Token
+from crashmanager.models import User as CMUser
 
 pytestmark = pytest.mark.django_db(transaction=True)
 
@@ -8,6 +10,9 @@ pytestmark = pytest.mark.django_db(transaction=True)
 @pytest.fixture
 def fm_user():
     user = User.objects.create_user('fuzzmanager', 'test@example.com', 'test')
+    content_type = ContentType.objects.get_for_model(CMUser)
+    user.user_permissions.add(Permission.objects.get(content_type=content_type, codename='view_ec2spotmanager'))
+    user.user_permissions.add(Permission.objects.get(content_type=content_type, codename='view_crashmanager'))
     user.password_raw = 'test'
 
     (token, created) = Token.objects.get_or_create(user=user)
