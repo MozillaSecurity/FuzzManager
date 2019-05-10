dc3131cef7a970abda474c7fd2750483a3053f37
Jesse Schwartzentruber <truber@mozilla.com>
2019-05-10 16:12:50 -0400

    Use redis as broker and backend for Celery.
    Redis is now a stable broker, we use it elsewhere anyways, and it is
    more efficient for synchronization takes (celery.chain/celery.chord).
    
    Setting a backend is required to use celery.chord.

--- server/celeryconf.py
+++ server/celeryconf.py
@@ -6,5 +6,5 @@ import os
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings_nondebug')
 
-app = Celery('tasks', broker='amqp://')
+app = Celery('tasks')
 app.config_from_object('django.conf:settings', namespace='CELERY')
