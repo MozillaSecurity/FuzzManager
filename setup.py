f1d1312bce8e8e23761ff909557cf767cf6c5373
Jesse Schwartzentruber <truber@mozilla.com>
2019-12-02 09:46:18 -0500

    Use tox for running pytest.
    Tox automates creating a virtual environment for running tests in
    isolation.

--- setup.py
+++ setup.py
@@ -2,26 +2,7 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    setup(author='Christian Holler',
-          classifiers=['Intended Audience :: Developers',
-                       'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-                       'Operating System :: OS Independent',
-                       'Programming Language :: Python',
-                       'Programming Language :: Python :: 2',
-                       'Programming Language :: Python :: 2.7',
-                       'Programming Language :: Python :: 3',
-                       'Programming Language :: Python :: 3.4',
-                       'Programming Language :: Python :: 3.5',
-                       'Programming Language :: Python :: 3.6',
-                       'Topic :: Software Development :: Testing',
-                       'Topic :: Security'],
-          description='A fuzzing management tools collection',
-          install_requires=['fasteners>=0.14.1', 'numpy>=1.11.2,<1.17', 'requests>=2.20.1', 'six>=1.12.0'],
-          keywords='fuzz fuzzing security test testing',
-          license='MPL 2.0',
-          maintainer='Mozilla Fuzzing Team',
-          maintainer_email='fuzzing@mozilla.com',
-          name='FuzzManager',
-          packages=['Collector', 'CovReporter', 'EC2Reporter', 'FTB', 'FTB.Running', 'FTB.Signatures', 'Reporter'],
-          url='https://github.com/MozillaSecurity/FuzzManager',
-          version='0.3.1')
+    setup(
+        use_scm_version=True,
+        setup_requires=['setuptools_scm'],
+    )
