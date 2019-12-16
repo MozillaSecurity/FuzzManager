a2d82bfaafeba94611cb43de41dd00fbd2beec30
Jesse Schwartzentruber <truber@mozilla.com>
2019-12-16 11:52:52 -0500

    Minimum setuptools requirement is v30.3.
    The global version in Ubuntu 16.04 (xenial) is 20.7, so this requires
    setuptools to be updated (eg. with `pip install -U setuptools`) for the
    install to work.

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         use_scm_version=True,
-        setup_requires=['setuptools_scm'],
+        setup_requires=['setuptools>=30.3', 'setuptools_scm'],
     )
