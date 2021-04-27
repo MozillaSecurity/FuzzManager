7895a137b4418f427ddd1877df21ccc01ed5173b
Eva Bardou <ebardou@teklia.com>
2021-04-27 17:43:14 +0200

    Add a JavaScript build system for Vue.js (#650)

---
+++ server/frontend/webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(__dirname, 'src', 'main.js'),
+
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '',
+    },
+
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+            }
+        ]
+    }
+}
--- server/server/settings.py
+++ server/server/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = (
     'server.middleware.ExceptionLoggingMiddleware',
     'server.middleware.CheckAppPermissionsMiddleware',
     #'livesync.core.middleware.DjangoLiveSyncMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 
@@ -187,6 +188,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_ROOT = '/path/to/static/'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/dist')
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
