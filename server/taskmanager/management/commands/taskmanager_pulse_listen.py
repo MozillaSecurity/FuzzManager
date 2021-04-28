c5f9297ec2f6e915455970e0f308f7098ed04b97
Jesse Schwartzentruber <truber@mozilla.com>
2021-04-28 10:10:57 -0400

    Fix kombu message handling in taskmanager pulse monitor.

--- server/taskmanager/management/commands/taskmanager_pulse_listen.py
+++ server/taskmanager/management/commands/taskmanager_pulse_listen.py
@@ -18,21 +18,25 @@ class TaskClusterConsumer(GenericConsumer):
         repo_slug = Path(settings.TC_FUZZING_CFG_REPO.split(":", 1)[1])
         org = repo_slug.parent
         repo = repo_slug.stem
+        exchanges = [
+            "exchange/taskcluster-queue/v1/task-pending",
+            "exchange/taskcluster-queue/v1/task-running",
+            "exchange/taskcluster-queue/v1/task-completed",
+            "exchange/taskcluster-queue/v1/task-failed",
+            "exchange/taskcluster-queue/v1/task-exception",
+            "exchange/taskcluster-github/v1/push",
+        ]
+        topics = ["primary.#.proj-%s.#" % settings.TC_PROJECT] * 5 + [
+            "primary.%s.%s" % (org, repo),
+        ]
         super().__init__(
             PulseConfiguration(**kwargs),
-            [
-                "exchange/taskcluster-queue/v1/task-pending",
-                "exchange/taskcluster-queue/v1/task-running",
-                "exchange/taskcluster-queue/v1/task-completed",
-                "exchange/taskcluster-queue/v1/task-failed",
-                "exchange/taskcluster-queue/v1/task-exception",
-                "exchange/taskcluster-github/v1/push",
-            ],
-            topic=["primary.#.proj-%s.#" % settings.TC_PROJECT] * 5 + [
-                "primary.%s.%s" % (org, repo),
-            ],
+            exchanges,
+            topic=topics,
             **kwargs,
         )
+        for exchange, topic in zip(exchanges, topics):
+            LOG.info("listening on %s for %s", exchange, topic)
 
 
 class Command(BaseCommand):
@@ -40,14 +44,15 @@ class Command(BaseCommand):
             "and schedule celery tasks to handle them")
 
     def callback(self, body, msg):
-        if msg.exchange.startswith("exchange/taskcluster-queue/v1/task-"):
+        LOG.info("got %s", msg.delivery_info["exchange"])
+        if msg.delivery_info["exchange"].startswith("exchange/taskcluster-queue/v1/task-"):
             update_task.delay(body)
             return
-        if msg.exchange == "exchange/taskcluster-github/v1/push":
+        if msg.delivery_info["exchange"] == "exchange/taskcluster-github/v1/push":
             if body["body"]["ref"] == "refs/heads/master":
                 update_pool_defns.delay()
             return
-        raise RuntimeError("Unhandled message: %s" % (msg.exchange,))
+        raise RuntimeError("Unhandled message: %s" % (msg.delivery_info["exchange"],))
 
     def handle(self, *args, **options):
         TaskClusterConsumer(
