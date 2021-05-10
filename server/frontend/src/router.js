0bed751dd20094e9e938ea91cd136c615a0617a8
Eva Bardou <ebardou@teklia.com>
2021-05-10 12:03:11 +0200

    Add comment

--- server/frontend/src/router.js
+++ server/frontend/src/router.js
@@ -4,6 +4,7 @@ import List from "./components/Crashes/List.vue";
 
 const routes = [
   {
+    // Be careful to keep this route up-to-date with the one in server/crashmanager/urls.py
     path: "/crashmanager/crashes2",
     name: "crashes-list",
     component: List,
