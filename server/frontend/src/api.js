371eaaa4b313aa8f750f52505622c6fd90b3f823
EvaBardou <eva.bardou38@gmail.com>
2021-05-25 11:20:47 +0200

    Add a component to add/edit/remove a Bugzilla API key in localStorage (#679)

--- server/frontend/src/api.js
+++ server/frontend/src/api.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 
-axios.defaults.xsrfCookieName = "csrftoken";
-axios.defaults.xsrfHeaderName = "X-CSRFToken";
-axios.defaults.withCredentials = true;
+const mainAxios = axios.create({
+  xsrfCookieName: "csrftoken",
+  xsrfHeaderName: "X-CSRFToken",
+  withCredentials: true,
+});
 
 export const retrieveBucket = async (id) =>
-  (await axios.get(`/crashmanager/rest/buckets/${id}/`)).data;
+  (await mainAxios.get(`/crashmanager/rest/buckets/${id}/`)).data;
 
 export const createBucket = async ({ params, ...data }) =>
-  (await axios.post("/crashmanager/rest/buckets/", data, { params })).data;
+  (await mainAxios.post("/crashmanager/rest/buckets/", data, { params })).data;
 
 export const updateBucket = async ({ id, params, ...data }) =>
-  (await axios.patch(`/crashmanager/rest/buckets/${id}/`, data, { params }))
+  (await mainAxios.patch(`/crashmanager/rest/buckets/${id}/`, data, { params }))
     .data;
 
 export const listCrashes = async (params) =>
-  (await axios.get("/crashmanager/rest/crashes/", { params })).data;
+  (await mainAxios.get("/crashmanager/rest/crashes/", { params })).data;
