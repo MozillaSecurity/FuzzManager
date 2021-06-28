72bc1d2056ea35bae8d1841fffab8a35282e6014
Eva Bardou <ebardou@teklia.com>
2021-06-28 13:30:40 +0200

    Add push Notification support and retrieve unread notifications count every 60s

--- server/server/urls.py
+++ server/server/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from notifications import views as notifications_views
 from .views import index, login
 
 
@@ -20,6 +21,12 @@ urlpatterns = [
     url(r'^crashmanager/', include('crashmanager.urls')),
     url(r'^taskmanager/', include('taskmanager.urls')),
     url(r'^ec2spotmanager/', include('ec2spotmanager.urls')),
+    url('inbox/notifications/', include(([
+        url(r'^unread/$', notifications_views.UnreadNotificationsList.as_view(), name='unread'),
+        url(r'^mark-all-as-read/$', notifications_views.mark_all_as_read, name='mark_all_as_read'),
+        url(r'^api/unread_count/$', notifications_views.live_unread_notification_count,
+            name='live_unread_notification_count'),
+    ], 'notifications'))),
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
