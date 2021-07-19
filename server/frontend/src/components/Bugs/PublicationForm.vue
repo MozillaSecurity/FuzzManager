a488c731541321745706cc64a4d4039ca351780e
Eva Bardou <ebardou@teklia.com>
2021-07-19 17:41:30 +0200

    Fix binary testcases loading/encoding on browser side

--- server/frontend/src/components/Bugs/PublicationForm.vue
+++ server/frontend/src/components/Bugs/PublicationForm.vue
@@ -780,12 +780,14 @@ export default {
           let content = this.testCaseContent;
           // If the testcase is binary we need to download it first
           if (this.entry.testcase_isbinary) {
-            content = await api.retrieveCrashTestCase(this.entry.id);
+            content = await api.retrieveCrashTestCaseBinary(this.entry.id);
           }
 
           payload = {
             ids: [this.createdBugId],
-            data: Base64.encode(content),
+            data: this.entry.testcase_isbinary
+              ? Base64.fromUint8Array(content)
+              : Base64.encode(content),
             file_name: this.entry.testcase,
             summary: "Testcase",
             content_type: this.entry.testcase_isbinary
