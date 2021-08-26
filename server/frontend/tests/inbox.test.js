a10a035671f311993f9ccb4bb87fdc53be66a47e
Eva Bardou <ebardou@teklia.com>
2021-08-26 12:03:36 +0200

    Fix linting

--- server/frontend/tests/inbox.test.js
+++ server/frontend/tests/inbox.test.js
@@ -1,7 +1,7 @@
-import {render, fireEvent} from '@testing-library/vue'
-import Inbox from '../src/components/Notifications/Inbox.vue'
+import { render } from "@testing-library/vue";
+import Inbox from "../src/components/Notifications/Inbox.vue";
 
-test('inbox has no notifications', async () => {
-  const {getByText} = render(Inbox)
-  getByText('Unread notifications')
-})
+test("inbox has no notifications", async () => {
+  const { getByText } = render(Inbox);
+  getByText("Unread notifications");
+});
