d07a04d4fbb92f3dfacc7a27ca6f09effed69800
Jesse Schwartzentruber <truber@mozilla.com>
2022-02-22 16:38:57 -0500

    Update project configuration to use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+"""setuptools install script"""
+
+import site
+
 from setuptools import setup
 
-if __name__ == '__main__':
-    setup(
-        use_scm_version=True,
-        setup_requires=['setuptools>=30.3', 'setuptools_scm'],
-    )
+site.ENABLE_USER_SITE = True
+
+
+if __name__ == "__main__":
+    setup()
