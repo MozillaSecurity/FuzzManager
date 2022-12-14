beae265354b726b30a7256fefe2602a5b41ba839
Jesse Schwartzentruber <truber@mozilla.com>
2022-12-14 12:24:27 -0500

    Add Python 3.11 CI (client only)

--- conftest.py
+++ conftest.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 def pytest_ignore_collect(path, config):
     # Django 4.1 requires 3.8
-    if sys.version_info < (3, 8):
+    # 3.11 causes an ImportError in vine (via celery)
+    if sys.version_info < (3, 8) or sys.version_info >= (3, 11):
         rel_path = Path(path).relative_to(config.rootdir)
         if rel_path.parts[0] in {
             "server",
