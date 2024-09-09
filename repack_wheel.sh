58b9567314b2a331b016e76627592d1a4d9ab61f
Jesse Schwartzentruber <truber@mozilla.com>
2024-09-09 15:32:56 -0400

    Initial rename to WebCompatManager

--- repack_wheel.sh
+++ repack_wheel.sh
@@ -7,9 +7,9 @@ set -e -x
 # The 'extras' are only useful with the full source distribution.
 #
 
-WORK="$(mktemp -d -t fm-twine-repack-XXXXXX)"
-wheel unpack -d "$WORK" dist/FuzzManager-*.whl
-grep -Ev "^(Provides-Extra: .*|Requires-Dist: .* ; extra == '.*')$" "$WORK"/FuzzManager-*/FuzzManager-*.dist-info/METADATA > "$WORK"/METADATA.new
-mv "$WORK"/METADATA.new "$WORK"/FuzzManager-*/FuzzManager-*.dist-info/METADATA
-wheel pack -d dist "$WORK"/FuzzManager-*
+WORK="$(mktemp -d -t wcm-twine-repack-XXXXXX)"
+wheel unpack -d "$WORK" dist/WebCompatManager-*.whl
+grep -Ev "^(Provides-Extra: .*|Requires-Dist: .* ; extra == '.*')$" "$WORK"/WebCompatManager-*/WebCompatManager-*.dist-info/METADATA > "$WORK"/METADATA.new
+mv "$WORK"/METADATA.new "$WORK"/WebCompatManager-*/WebCompatManager-*.dist-info/METADATA
+wheel pack -d dist "$WORK"/WebCompatManager-*
 rm -rf "$WORK"
