d70f0372b05e120d901a9e87729b4d3f1f7d2b35
Jesse Schwartzentruber <truber@mozilla.com>
2024-09-13 09:45:26 -0400

    Update deprecated test fixture

--- conftest.py
+++ conftest.py
@@ -10,7 +10,6 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 import importlib
 import sys
-from pathlib import Path
 
 # Currently, the version of django used here is not supported by python 3.12
 # Check that django is available before setting the pytest_plugins otherwise the tests
@@ -19,10 +18,10 @@ if importlib.util.find_spec("django"):
     pytest_plugins = ["covmanager.tests.conftest"]
 
 
-def pytest_ignore_collect(path, config):
+def pytest_ignore_collect(collection_path, config):
     # 3.12 not supported yet
     if sys.version_info >= (3, 12):
-        rel_path = Path(path).relative_to(config.rootdir)
+        rel_path = collection_path.relative_to(config.rootdir)
         if rel_path.parts[0] in {
             "server",
             "Collector",
