194c0b8d152108e60ac43da05752513eb074c775
gitstart_bot <gitstart_bot@users.noreply.github.com>
2025-02-21 13:08:17 +0000

    Add OS and Platform columns to taskmanager pool view

--- server/frontend/src/components/Pools/List.vue
+++ server/frontend/src/components/Pools/List.vue
@@ -3,78 +3,134 @@
     <div class="panel-heading">
       <i class="bi bi-card-list"></i> Fuzzing Pools
     </div>
-    <table class="table table-condensed table-hover table-bordered table-db">
-      <thead>
-        <tr>
-          <th
-            :class="{
-              active:
-                sortKeys.includes('pool_id') || sortKeys.includes('-pool_id'),
-            }"
-            width="25px"
-            @click.exact="sortBy('pool_id')"
-            @click.ctrl.exact="addSort('pool_id')"
-          >
-            ID
-          </th>
-          <th
-            :class="{
-              active:
-                sortKeys.includes('pool_name_isort') ||
-                sortKeys.includes('-pool_name_isort'),
-            }"
-            width="100px"
-            @click.exact="sortBy('pool_name_isort')"
-            @click.ctrl.exact="addSort('pool_name_isort')"
-          >
-            Name
-          </th>
-          <th
-            :class="{
-              active:
-                sortKeys.includes('running') || sortKeys.includes('-running'),
-            }"
-            width="75px"
-            @click.exact="sortBy('running')"
-            @click.ctrl.exact="addSort('running')"
-          >
-            # of Tasks (Running/Requested)
-          </th>
-          <th
-            :class="{
-              active:
-                sortKeys.includes('status') || sortKeys.includes('-status'),
-            }"
-            width="150px"
-            @click.exact="sortBy('status')"
-            @click.ctrl.exact="addSort('status')"
-          >
-            Status
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="pool in orderedPools" :key="pool.id">
-          <td>
-            <a :href="pool.view_url">{{ pool.pool_id }}</a>
-          </td>
-          <td>{{ pool.pool_name }}</td>
-          <td>{{ pool.running }}/{{ pool.size }}</td>
-          <td>
-            <span
-              class="label"
+    <div class="table-responsive">
+      <table class="table table-condensed table-hover table-bordered table-db">
+        <thead>
+          <tr>
+            <th
               :class="{
-                'label-warning': pool.status === 'partial',
-                'label-success': pool.status === 'healthy',
-                'label-danger': pool.status === 'idle',
-                'label-default': pool.status === 'disabled',
+                active:
+                  sortKeys.includes('pool_id') || sortKeys.includes('-pool_id'),
               }"
-              >{{ pool.status }}</span
+              width="25px"
+              @click.exact="sortBy('pool_id')"
+              @click.ctrl.exact="addSort('pool_id')"
             >
-          </td>
-        </tr>
-      </tbody>
-    </table>
+              ID
+            </th>
+            <th
+              :class="{
+                active:
+                  sortKeys.includes('pool_name_isort') ||
+                  sortKeys.includes('-pool_name_isort'),
+              }"
+              width="100px"
+              @click.exact="sortBy('pool_name_isort')"
+              @click.ctrl.exact="addSort('pool_name_isort')"
+            >
+              Name
+            </th>
+            <th
+              :class="{
+                active:
+                  sortKeys.includes('running') || sortKeys.includes('-running'),
+              }"
+              width="75px"
+              @click.exact="sortBy('running')"
+              @click.ctrl.exact="addSort('running')"
+            >
+              # of Tasks (Running/Requested)
+            </th>
+            <th
+              :class="{
+                active:
+                  sortKeys.includes('status') || sortKeys.includes('-status'),
+              }"
+              width="150px"
+              @click.exact="sortBy('status')"
+              @click.ctrl.exact="addSort('status')"
+            >
+              Status
+            </th>
+            <th
+              :class="{
+                active: sortKeys.includes('cpu') || sortKeys.includes('-cpu'),
+              }"
+              width="150px"
+              @click.exact="sortBy('cpu')"
+              @click.ctrl.exact="addSort('cpu')"
+            >
+              Platform
+            </th>
+            <th
+              :class="{
+                active:
+                  sortKeys.includes('platform') ||
+                  sortKeys.includes('-platform'),
+              }"
+              width="25px"
+              @click.exact="sortBy('platform')"
+              @click.ctrl.exact="addSort('platform')"
+            >
+              OS
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="pool in orderedPools" :key="pool.id">
+            <td>
+              <a :href="pool.view_url">{{ pool.pool_id }}</a>
+            </td>
+            <td>{{ pool.pool_name }}</td>
+            <td>{{ pool.running }}/{{ pool.size }}</td>
+            <td>
+              <span
+                class="label"
+                :class="{
+                  'label-warning': pool.status === 'partial',
+                  'label-success': pool.status === 'healthy',
+                  'label-danger': pool.status === 'idle',
+                  'label-default': pool.status === 'disabled',
+                }"
+                >{{ pool.status }}</span
+              >
+            </td>
+            <td>{{ pool.cpu }}</td>
+            <td>
+              <img
+                v-if="pool.platform === 'linux'"
+                width="16px"
+                height="16px"
+                alt="Linux"
+                :src="staticLogo('linux')"
+              />
+              <img
+                v-else-if="pool.platform === 'macosx'"
+                width="16px"
+                height="16px"
+                alt="MacOS"
+                :src="staticLogo('macosx')"
+              />
+              <img
+                v-else-if="pool.platform === 'windows'"
+                width="16px"
+                height="16px"
+                alt="Windows"
+                :src="staticLogo('windows')"
+              />
+              <img
+                v-else-if="pool.platform === 'android'"
+                width="16px"
+                height="16px"
+                alt="Android"
+                :src="staticLogo('android')"
+              />
+              <span v-else>{{ pool.platform }}</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -90,7 +146,14 @@ export default defineComponent({
   mixins: [multiSort],
   setup() {
     const defaultSortKeys = ["pool_name_isort"];
-    const validSortKeys = ["pool_id", "pool_name_isort", "running", "status"];
+    const validSortKeys = [
+      "pool_id",
+      "pool_name_isort",
+      "running",
+      "platform",
+      "cpu",
+      "status",
+    ];
     const loading = ref(true);
     const pools = ref(null);
     const sortKeys = ref([...defaultSortKeys]);
@@ -129,12 +192,17 @@ export default defineComponent({
 
     fetch();
 
+    const staticLogo = (name) => {
+      return window.location.origin + "/static/img/os/" + name + ".png";
+    };
+
     return {
       loading,
       pools,
       sortKeys,
       validSortKeys,
       defaultSortKeys,
+      staticLogo,
     };
   },
   computed: {
