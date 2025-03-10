959958c6d591cefd76cc304510681d042a2a7379
gitstart <gitstart@gitstart.com>
2025-03-10 10:27:16 +0000

    chore: implement tools segmentation for crash entries

--- server/crashmanager/views.py
+++ server/crashmanager/views.py
@@ -1134,6 +1134,21 @@ class CrashEntryViewSet(
             obj.testcase.save()
         return Response(CrashEntrySerializer(obj).data)
 
+    def create(self, request, *args, **kwargs):
+        """Check user has access to tool before creation"""
+        tool_name = request.data.get('tool')
+        user = User.get_or_create_restricted(request.user)[0]
+        if user.restricted and tool_name:
+            allowed_tools = user.defaultToolsFilter.values_list('name', flat=True)
+            if not allowed_tools:
+                raise PermissionDenied({"message": "No tools assigned to user"})
+
+            if tool_name not in allowed_tools:
+                raise PermissionDenied(
+                    {"message": f"You don't have permission to use tool: {tool_name}"}
+                )
+        return super().create(request, *args, **kwargs)
+
 
 class BucketViewSet(
     mixins.CreateModelMixin,
