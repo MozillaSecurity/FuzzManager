67b404184dc40a73689e039b55fd9a377bd451ff
Jesse Schwartzentruber <truber@mozilla.com>
2025-04-01 16:33:38 -0400

    fix: Fix email and tool filter editing

--- server/crashmanager/tests/test_user_settings.py
+++ server/crashmanager/tests/test_user_settings.py
@@ -36,11 +36,11 @@ def test_user_settings_simple_get(client):
     response = client.get(path)
     assert response.status_code == requests.codes["ok"]
     assert list(response.context["bugzilla_providers"]) == []
-    assert response.context["user"] == User.objects.get(user__username="test")
 
 
-def test_user_settings_edit(client, cm):
+def test_user_settings_edit(client, cm, settings):
     """No errors are thrown in template"""
+    settings.ALLOW_EMAIL_EDITION = True
     tools = [Tool.objects.create(name="Tool #%d" % (i + 1)) for i in range(2)]
     providers = [
         cm.create_bugprovider(hostname="Provider #%d" % (i + 1)) for i in range(2)
@@ -80,3 +80,162 @@ def test_user_settings_edit(client, cm):
     assert user.inaccessible_bug
     assert user.bucket_hit
     assert user.tasks_failed
+
+
+def test_user_settings_clear_tool_filter(client, cm):
+    """tool filter should be clearable"""
+    tools = [Tool.objects.create(name="Tool #%d" % (i + 1)) for i in range(2)]
+    providers = [
+        cm.create_bugprovider(hostname="Provider #%d" % (i + 1)) for i in range(2)
+    ]
+    [cm.create_template(name="Template #%d" % (i + 1)) for i in range(2)]
+    user = User.objects.get(user__username="test")
+    user.defaultToolsFilter.add(tools[0])
+    user.defaultToolsFilter.add(tools[1])
+    user.save()
+    client.login(username="test", password="test")
+    response = client.post(
+        reverse("crashmanager:usersettings"),
+        data={
+            "defaultToolsFilter": [],
+            "defaultProviderId": providers[1].pk,
+            "defaultTemplateId": 1,
+            "email": "mynewemail@mozilla.com",
+            "inaccessible_bug": True,
+            "bucket_hit": True,
+            "tasks_failed": True,
+        },
+    )
+    LOG.debug(response)
+    # Redirecting to user settings when the action is successful
+    assert response.status_code == requests.codes["found"]
+    assert response.url == "/crashmanager/usersettings/"
+    user.refresh_from_db()
+    assert not list(user.defaultToolsFilter.all())
+
+
+def test_user_settings_restricted_no_edit_tool_filter(client, cm):
+    """restricted user should not be able to edit tool filter"""
+    tools = [Tool.objects.create(name="Tool #%d" % (i + 1)) for i in range(2)]
+    providers = [
+        cm.create_bugprovider(hostname="Provider #%d" % (i + 1)) for i in range(2)
+    ]
+    [cm.create_template(name="Template #%d" % (i + 1)) for i in range(2)]
+    user = User.objects.get(user__username="test-restricted")
+    user.defaultToolsFilter.add(tools[0])
+    user.save()
+    client.login(username="test", password="test")
+
+    # changed tool filter
+    response = client.post(
+        reverse("crashmanager:usersettings"),
+        data={
+            "defaultToolsFilter": [tools[1].pk],
+            "defaultProviderId": providers[1].pk,
+            "defaultTemplateId": 1,
+            "email": "mynewemail@mozilla.com",
+            "inaccessible_bug": True,
+            "bucket_hit": True,
+            "tasks_failed": True,
+        },
+    )
+    LOG.debug(response)
+    # Redirecting to user settings when the action is successful
+    assert response.status_code == requests.codes["found"]
+    assert response.url == "/crashmanager/usersettings/"
+    user.refresh_from_db()
+    assert list(user.defaultToolsFilter.all()) == [tools[0]]
+
+    # empty tool filter
+    response = client.post(
+        reverse("crashmanager:usersettings"),
+        data={
+            "defaultToolsFilter": [],
+            "defaultProviderId": providers[1].pk,
+            "defaultTemplateId": 1,
+            "email": "mynewemail@mozilla.com",
+            "inaccessible_bug": True,
+            "bucket_hit": True,
+            "tasks_failed": True,
+        },
+    )
+    LOG.debug(response)
+    # Redirecting to user settings when the action is successful
+    assert response.status_code == requests.codes["found"]
+    assert response.url == "/crashmanager/usersettings/"
+    user.refresh_from_db()
+    assert list(user.defaultToolsFilter.all()) == [tools[0]]
+
+    # omitted tool filter
+    response = client.post(
+        reverse("crashmanager:usersettings"),
+        data={
+            "defaultProviderId": providers[1].pk,
+            "defaultTemplateId": 1,
+            "email": "mynewemail@mozilla.com",
+            "inaccessible_bug": True,
+            "bucket_hit": True,
+            "tasks_failed": True,
+        },
+    )
+    LOG.debug(response)
+    # Redirecting to user settings when the action is successful
+    assert response.status_code == requests.codes["found"]
+    assert response.url == "/crashmanager/usersettings/"
+    user.refresh_from_db()
+    assert list(user.defaultToolsFilter.all()) == [tools[0]]
+
+
+def test_user_settings_no_edit_email(client, cm, settings):
+    """settings.ALLOW_EMAIL_EDITION=False disallows editing email"""
+    settings.ALLOW_EMAIL_EDITION = False
+    tools = [Tool.objects.create(name="Tool #%d" % (i + 1)) for i in range(2)]
+    providers = [
+        cm.create_bugprovider(hostname="Provider #%d" % (i + 1)) for i in range(2)
+    ]
+    [cm.create_template(name="Template #%d" % (i + 1)) for i in range(2)]
+    user = User.objects.get(user__username="test")
+    user.defaultToolsFilter.add(tools[0])
+    user.save()
+    assert user.user.email == "test@mozilla.com"
+
+    # changed email
+    client.login(username="test", password="test")
+    response = client.post(
+        reverse("crashmanager:usersettings"),
+        data={
+            "defaultToolsFilter": [tools[0].pk],
+            "defaultProviderId": providers[1].pk,
+            "defaultTemplateId": 1,
+            "email": "mynewemail@mozilla.com",
+            "inaccessible_bug": True,
+            "bucket_hit": True,
+            "tasks_failed": True,
+        },
+    )
+    LOG.debug(response)
+    # Redirecting to user settings when the action is successful
+    assert response.status_code == requests.codes["found"]
+    assert response.url == "/crashmanager/usersettings/"
+    user.refresh_from_db()
+    assert user.user.email == "test@mozilla.com"
+
+    # omitted email
+    response = client.post(
+        reverse("crashmanager:usersettings"),
+        data={
+            "defaultToolsFilter": [tools[0].pk],
+            "defaultProviderId": providers[1].pk,
+            "defaultTemplateId": 1,
+            "inaccessible_bug": True,
+            "bucket_hit": True,
+            "tasks_failed": True,
+        },
+    )
+    LOG.debug(response)
+    LOG.debug(response.context)
+    # no redirect, form errors
+    assert response.status_code == requests.codes["ok"]
+    assert "email" in response.context["form_errors"]
+    user.refresh_from_db()
+    assert user.user.email == "test@mozilla.com"
