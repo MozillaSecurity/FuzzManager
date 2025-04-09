0ca3251d53a156ed60f0e9f5afe9381e7623e64a
Jesse Schwartzentruber <truber@mozilla.com>
2025-04-09 15:21:47 -0400

    fix: don't require wheel filename to begin with "FuzzManager"
    Generated wheel filenames are lowercase now.

--- repack_wheel.sh
+++ repack_wheel.sh
@@ -8,8 +8,8 @@ set -e -x
 #
 
 WORK="$(mktemp -d -t fm-twine-repack-XXXXXX)"
-wheel unpack -d "$WORK" dist/FuzzManager-*.whl
-grep -Ev "^(Provides-Extra: .*|Requires-Dist: .* ; extra == '.*')$" "$WORK"/FuzzManager-*/FuzzManager-*.dist-info/METADATA > "$WORK"/METADATA.new
-mv "$WORK"/METADATA.new "$WORK"/FuzzManager-*/FuzzManager-*.dist-info/METADATA
-wheel pack -d dist "$WORK"/FuzzManager-*
+wheel unpack -d "$WORK" dist/*.whl
+grep -Ev "^(Provides-Extra: .*|Requires-Dist: .* ; extra == '.*')$" "$WORK"/*/*.dist-info/METADATA > "$WORK"/METADATA.new
+mv "$WORK"/METADATA.new "$WORK"/*/*.dist-info/METADATA
+wheel pack -d dist "$WORK"/*
 rm -rf "$WORK"
