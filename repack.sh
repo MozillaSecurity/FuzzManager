02f9506b9e8628e360e36e8deaacea8c5dde1aca
Jesse Schwartzentruber <truber@mozilla.com>
2025-04-09 16:57:36 -0400

    fix: remove URL requirements from source metadata

--- repack.sh
+++ repack.sh
@@ -13,3 +13,18 @@ grep -Ev "^(Provides-Extra: .*|Requires-Dist: .*;.* extra == .*)$" "$WORK"/*/*.d
 mv "$WORK"/METADATA.new "$WORK"/*/*.dist-info/METADATA
 wheel pack -d dist "$WORK"/*
 rm -rf "$WORK"
+
+#
+# Remove `@git+` requires from the source
+#
+# PyPI is now rejecting these.
+#
+
+WORK="$(mktemp -d -t fm-twine-repack-XXXXXX)"
+TAR="$(ls dist/*.tar.*)"
+tar -C "$WORK" -xvaf "$TAR"
+sed -Ei 's/^(Requires-Dist:\s*[A-Za-z0-9][A-Za-z0-9._-]*)\s*@\s*([^ ]+)(.*)$/\1\3/' "$WORK"/*/PKG-INFO
+FN="$(ls "$WORK")"
+rm "$TAR"
+tar -C "$WORK" -cvaf "$TAR" "$FN"
+rm -rf "$WORK"
