4f602c8997cf9efc4119324c247ed961892dd77b
Jesse Schwartzentruber <truber@mozilla.com>
2025-12-03 21:41:52 -0500

    feat(taskmanager): replace mozillapulse with kombu

--- server/taskmanager/management/commands/taskmanager_pulse_listen.py
+++ server/taskmanager/management/commands/taskmanager_pulse_listen.py
@@ -1,58 +1,81 @@
 from logging import getLogger
 from pathlib import Path
 
+from amqp.exceptions import PreconditionFailed
 from django.conf import settings
 from django.core.management import BaseCommand, CommandError  # noqa
-from mozillapulse.consumers import GenericConsumer, PulseConfiguration
+from kombu import Connection, Exchange, Queue, binding
+from kombu.mixins import ConsumerMixin
 
 from ...tasks import update_pool_defns, update_task
 
 LOG = getLogger("taskmanager.management.commands.listen")
+REPO_SLUG = Path(settings.TC_FUZZING_CFG_REPO.split(":", 1)[1])
 
 
-class TaskClusterConsumer(GenericConsumer):
-    def __init__(self, **kwargs):
-        repo_slug = Path(settings.TC_FUZZING_CFG_REPO.split(":", 1)[1])
-        org = repo_slug.parent
-        repo = repo_slug.stem
-        exchanges = [
-            "exchange/taskcluster-queue/v1/task-pending",
-            "exchange/taskcluster-queue/v1/task-running",
-            "exchange/taskcluster-queue/v1/task-completed",
-            "exchange/taskcluster-queue/v1/task-failed",
-            "exchange/taskcluster-queue/v1/task-exception",
-            "exchange/taskcluster-github/v1/push",
-        ]
-        topics = [f"primary.#.proj-{settings.TC_PROJECT}.#"] * 5 + [
-            f"primary.{org}.{repo}",
-        ]
-        super().__init__(
-            PulseConfiguration(**kwargs),
-            exchanges,
-            applabel=Path(__file__).stem,
-            durable=True,
-            expires=21600,
-            topic=topics,
-            **kwargs,
-        )
-        for exchange, topic in zip(exchanges, topics):
-            LOG.info("listening on %s for %s", exchange, topic)
-
-
-class Command(BaseCommand):
-    help = (
-        "Listens for Mozilla Pulse messages relating to TaskManager tasks, "
-        "and schedule celery tasks to handle them"
+class TaskClusterConsumer(ConsumerMixin):
+    queue = Queue(
+        bindings=[
+            binding(
+                Exchange("exchange/taskcluster-queue/v1/task-pending", type="topic"),
+                routing_key=f"primary.#.proj-{settings.TC_PROJECT}.#",
+            ),
+            binding(
+                Exchange("exchange/taskcluster-queue/v1/task-running", type="topic"),
+                routing_key=f"primary.#.proj-{settings.TC_PROJECT}.#",
+            ),
+            binding(
+                Exchange("exchange/taskcluster-queue/v1/task-completed", type="topic"),
+                routing_key=f"primary.#.proj-{settings.TC_PROJECT}.#",
+            ),
+            binding(
+                Exchange("exchange/taskcluster-queue/v1/task-failed", type="topic"),
+                routing_key=f"primary.#.proj-{settings.TC_PROJECT}.#",
+            ),
+            binding(
+                Exchange("exchange/taskcluster-queue/v1/task-exception", type="topic"),
+                routing_key=f"primary.#.proj-{settings.TC_PROJECT}.#",
+            ),
+            binding(
+                Exchange("exchange/taskcluster-github/v1/push"),
+                routing_key=f"primary.{REPO_SLUG.parent}.{REPO_SLUG.stem}",
+            ),
+        ],
+        name=f"queue/{settings.TC_PULSE_USERNAME}/{Path(__file__).stem}",
     )
 
-    def callback(self, body, msg):
-        if msg.delivery_info["exchange"].startswith(
+    def __init__(self, connection):
+        self.connection = connection
+
+    def get_consumers(self, Consumer, channel):
+        consumer = Consumer(
+            queues=self.queue, auto_declare=False, callbacks=[self.on_message]
+        )
+        queue = consumer.queues[0]
+        try:
+            queue.queue_declare()
+        except PreconditionFailed as exc:
+            LOG.warning(
+                "failed to declare queue, trying to delete first (error: %s)", exc
+            )
+            queue.delete()
+            queue.queue_declare()
+        for bing in consumer.queues[0].bindings:
+            bing.exchange(channel).declare(passive=True)
+            queue.bind_to(exchange=bing.exchange, routing_key=bing.routing_key)
+        return [consumer]
+
+    def on_consume_ready(self, connection, channel, consumers, **kwargs):
+        LOG.info("pulse listener starting")
+
+    def on_message(self, body, message):
+        if message.delivery_info["exchange"].startswith(
             "exchange/taskcluster-queue/v1/task-"
         ):
             LOG.info(
                 "%s on %s for %s",
-                msg.delivery_info["routing_key"],
-                msg.delivery_info["exchange"],
+                message.delivery_info["routing_key"],
+                message.delivery_info["exchange"],
                 body["status"]["taskId"],
             )
             if body["status"]["schedulerId"] == "audit":
@@ -63,36 +86,39 @@ class Command(BaseCommand):
                 )
             else:
                 update_task.delay(body)
-            msg.ack()
+            message.ack()
             return
-        if msg.delivery_info["exchange"] == "exchange/taskcluster-github/v1/push":
+        if message.delivery_info["exchange"] == "exchange/taskcluster-github/v1/push":
             LOG.info(
                 "%s on %s for %s",
-                msg.delivery_info["routing_key"],
-                msg.delivery_info["exchange"],
+                message.delivery_info["routing_key"],
+                message.delivery_info["exchange"],
                 body["body"]["ref"],
             )
             if body["body"]["ref"] == "refs/heads/master":
                 update_pool_defns.delay()
-            msg.ack()
+            message.ack()
             return
         raise RuntimeError(
-            "Unhandled message: %s on %s"
-            % (
-                msg.delivery_info["routing_key"],
-                msg.delivery_info["exchange"],
-            )
+            f"Unhandled message: {message.delivery_info['routing_key']}"
+            f" on {message.delivery_info['exchange']}"
         )
 
+
+class Command(BaseCommand):
+    help = (
+        "Listens for Mozilla Pulse messages relating to TaskManager tasks, "
+        "and schedule celery tasks to handle them"
+    )
+
     def handle(self, *args, **options):
-        LOG.info("pulse listener starting")
         try:
-            TaskClusterConsumer(
-                vhost=settings.TC_PULSE_VHOST,
-                user=settings.TC_PULSE_USERNAME,
-                password=settings.TC_PULSE_PASSWORD,
-                callback=self.callback,
-            ).listen()
+            with Connection(
+                f"amqp://{settings.TC_PULSE_USERNAME}:{settings.TC_PULSE_PASSWORD}"
+                f"@pulse.mozilla.org:5671/{settings.TC_PULSE_VHOST}?ssl=1"
+            ) as connection:
+                worker = TaskClusterConsumer(connection)
+                worker.run()
         except Exception:
             LOG.exception("pulse listener raised")
             raise
