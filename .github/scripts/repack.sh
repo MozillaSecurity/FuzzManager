60e9573c9be7877803d23a843fcb4c297627e620
Jesse Schwartzentruber <truber@mozilla.com>
2025-12-11 11:30:33 -0500

    ci: use Github Actions

--- .github/scripts/repack.sh
+++ .github/scripts/repack.sh
@@ -8,10 +8,10 @@ set -e -x
 #
 
 WORK="$(mktemp -d -t fm-twine-repack-XXXXXX)"
-wheel unpack -d "$WORK" dist/*.whl
+uvx wheel unpack -d "$WORK" dist/*.whl
 grep -Ev "^(Provides-Extra: .*|Requires-Dist: .*;.* extra == .*)$" "$WORK"/*/*.dist-info/METADATA > "$WORK"/METADATA.new
 mv "$WORK"/METADATA.new "$WORK"/*/*.dist-info/METADATA
-wheel pack -d dist "$WORK"/*
+uvx wheel pack -d dist "$WORK"/*
 rm -rf "$WORK"
 
 #
